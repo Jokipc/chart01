7e5dc7c0c59bd33f11ff2cec5b0f7da1c7f83248
jokipc <abagussaputra@gmail.com>
2022-01-14 00:34:02 +0700

    update

---
+++ application/views/rank.php
@@ -0,0 +1,46 @@
+<html lang="en"	dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet"	href="<?php echo base_url() ;?>template/dist/css/style.css">
+		<title>Air Balons</title>
+	</head>
+	<body>
+	<img src="<?php echo base_url() ;?>template/dist/img/a.png" class="b b1" alt="">
+	<img src="<?php echo base_url() ;?>template/dist/img/aa.png" class="b b2" alt="">
+	<img src="<?php echo base_url() ;?>template/dist/img/aaa.png" class="b b3" alt="">
+	
+	<div class="c c1">
+		<div class="cloud">
+		</div>
+	</div>
+	<div class="c c2">
+		<div class="cloud">
+		</div>
+	</div>
+	<div class="c c3">
+		<div class="cloud">
+		</div>
+	</div>
+	<div class="c c4">
+		<div class="cloud">
+		</div>
+	</div>
+	<div class="c c5">
+		<div class="cloud">
+		</div>
+	</div>
+	<div class="c c6">
+		<div class="cloud">
+		</div>
+	</div>
+	
+	<div class="ground">
+		<div class+"t t1">
+		<span></span>
+		<span></span>
+	</div>
+	
+	</body>
+
+
+</html>
