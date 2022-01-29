f5da15bfbcd0baae0d994ab27d045cf8978d825e
jokipc <abagussaputra@gmail.com>
2022-01-29 23:59:08 +0700

    update2

--- application/views/saving/saving_list.php
+++ application/views/saving/saving_list.php
@@ -28,22 +28,7 @@
                 </div>
             </div>
             <div class="col-md-1 text-right">
-             <form action="<?php echo site_url('saving/index'); ?>" class="form-inline" method="get">
-                    <div class="input-group">
-                        <input type="text" class="form-control" name="q" value="<?php echo $q; ?>">
-                        <span class="input-group-btn">
-                            <?php 
-                                if ($q <> '')
-                                {
-                                    ?>
-                                    <a href="<?php echo site_url('saving'); ?>" class="btn btn-default">Reset</a>
-                                    <?php
-                                }
-                            ?>
-                          <button class="btn btn-primary" type="submit">Search</button>
-                        </span>
-                    </div>
-                    <table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
+
             </div>
             <div class="col-md-3 text-right" style="overflow-x:auto;">
                
