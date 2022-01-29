8e06eec2f0adf0bf93cdaa38a0b47a30865d7fda
jokipc <abagussaputra@gmail.com>
2022-01-30 00:12:56 +0700

    update2

--- application/views/saving/saving_list.php
+++ application/views/saving/saving_list.php
@@ -10,15 +10,16 @@
     <body>
     <div class="content-wrapper" style="min-height: 955.807px;"> 
     <section class="content">
-     <?php $this->load->view('templates/footer'); ?> 
+      
       
  
   
     </section>
+    </div>
     </body>
 </html>
 
-
+<?php $this->load->view('templates/footer'); ?>
 <?php else: ?>
 <br>
 <br>
