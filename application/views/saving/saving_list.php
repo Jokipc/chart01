7bf678b06edf529ed8e46ce7ed5473aae1890848
jokipc <abagussaputra@gmail.com>
2022-01-30 00:18:19 +0700

    update2

--- application/views/saving/saving_list.php
+++ application/views/saving/saving_list.php
@@ -17,6 +17,7 @@
 		        <th>Pn</th>
 		        <th>Tgl</th>
 		        <th>Norek</th>
+		        <th>Norek</th>
 				<th>Action</th>
             </tr><?php
             foreach ($saving_data as $saving)
@@ -26,6 +27,7 @@
 			<td width="80px"><?php echo ++$start ?></td>
 			<td><?php echo $saving->pn ?></td>
 			<td><?php echo $saving->tgl ?></td>
+			<td><?php echo $saving->norek ?></td>            
 			<td><?php echo $saving->norek ?></td>            
 			<td style="text-align:center" width="200px">
 				<?php 
