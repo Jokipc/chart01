6f182898764c8e2d7d6db9e65cda18a0416648e8
jokipc <abagussaputra@gmail.com>
2022-01-30 00:16:16 +0700

    update2

--- application/views/saving/saving_list.php
+++ application/views/saving/saving_list.php
@@ -11,6 +11,34 @@
     <div class="content-wrapper" style="min-height: 955.807px;"> 
     <section class="content">
       
+    <table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
+            <tr>
+                <th>No</th>
+		        <th>Pn</th>
+		        <th>Tgl</th>
+		        <th>Norek</th>
+				<th>Action</th>
+            </tr><?php
+            foreach ($saving_data as $saving)
+            {
+                ?>
+            <tr>
+			<td width="80px"><?php echo ++$start ?></td>
+			<td><?php echo $saving->pn ?></td>
+			<td><?php echo $saving->tgl ?></td>
+			<td><?php echo $saving->norek ?></td>
+            <td><?php echo $saving->norek ?></td>
+			<td style="text-align:center" width="200px">
+				<?php 
+				
+				echo anchor(site_url('saving/delete/'.$saving->id),'Delete','onclick="javasciprt: return confirm(\'Are You Sure ?\')"'); 
+				?>
+			</td>
+		    </tr>
+                <?php
+            }
+            ?>
+    </table>
       
  
   
