1206efcfaf1a8900e6623a6506c4521ad33600f2
jokipc <abagussaputra@gmail.com>
2022-01-29 23:44:09 +0700

    update2

--- application/views/saving/saving_list.php
+++ application/views/saving/saving_list.php
@@ -29,7 +29,7 @@
             </div>
             <div class="col-md-1 text-right">
             </div>
-            <div class="col-md-3 text-right">
+            <div class="col-md-3 text-right" style="overflow-x:auto;">
                 <form action="<?php echo site_url('saving/index'); ?>" class="form-inline" method="get">
                     <div class="input-group">
                         <input type="text" class="form-control" name="q" value="<?php echo $q; ?>">
