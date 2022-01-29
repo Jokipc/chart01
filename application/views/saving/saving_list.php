b9e97c36c88d7daccaa51f818272c6f4b8b0bc52
jokipc <abagussaputra@gmail.com>
2022-01-30 00:04:36 +0700

    update2

--- application/views/saving/saving_list.php
+++ application/views/saving/saving_list.php
@@ -1,7 +1,6 @@
 <?php if($this->session->userdata('id_level')==='1'):?> <!doctype html>
 <html>
     <head>
-
 <?php $this->load->view('templates/header'); ?>
 <?php $this->load->view('templates/sidebaradmin'); ?>
 <?php $this->load->view('templates/meta'); ?>
@@ -9,7 +8,6 @@
     </head>
 
     <body>
-
     <div class="content-wrapper" style="min-height: 955.807px;">
   
     
@@ -18,9 +16,18 @@
         
 
         <h2 style="margin-top:0px">Saving List</h2>
-        
         <div class="row" style="margin-bottom: 10px">
-          <div class="col-md-3 text-right" style="overflow-x:auto;">
+            <div class="col-md-4">
+                
+            </div>
+            <div class="col-md-4 text-center">
+                <div style="margin-top: 8px" id="message">
+                    <?php echo $this->session->userdata('message') <> '' ? $this->session->userdata('message') : ''; ?>
+                </div>
+            </div>
+            <div class="col-md-1 text-right">
+            </div>
+            <div class="col-md-3 text-right">
                 <form action="<?php echo site_url('saving/index'); ?>" class="form-inline" method="get">
                     <div class="input-group">
                         <input type="text" class="form-control" name="q" value="<?php echo $q; ?>">
@@ -36,15 +43,17 @@
                           <button class="btn btn-primary" type="submit">Search</button>
                         </span>
                     </div>
-                    <table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
+                </form>
+            </div>
+        </div>
+
+        <table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
             <tr>
-        <th>No</th>
+                <th>No</th>
 		<th>Pn</th>
-		<th>Tanggal</th>
-		<th>No Rek</th>
+		<th>Tgl</th>
+		<th>Norek</th>
 		<th>Action</th>
-		
-		
             </tr><?php
             foreach ($saving_data as $saving)
             {
@@ -54,8 +63,6 @@
 			<td><?php echo $saving->pn ?></td>
 			<td><?php echo $saving->tgl ?></td>
 			<td><?php echo $saving->norek ?></td>
-			
-			
 			<td style="text-align:center" width="200px">
 				<?php 
 				
@@ -67,21 +74,6 @@
             }
             ?>
         </table>
-                </form>
-            </div>    
-            <div class="col-md-4 text-center">
-                <div style="margin-top: 8px" id="message">
-                    <?php echo $this->session->userdata('message') <> '' ? $this->session->userdata('message') : ''; ?>
-                </div>
-            </div>
-           
-          
-
-            
-        
-
-        
-
         <div class="row">
             <div class="col-md-6">
                 <a href="#" class="btn btn-primary">Total Record : <?php echo $total_rows ?></a>
