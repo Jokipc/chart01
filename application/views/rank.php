c60f4bcd7ca510d893b6480401a5ad28cd7bafcd
jokipc <abagussaputra@gmail.com>
2022-02-02 00:53:09 +0700

    rank

--- application/views/rank.php
+++ application/views/rank.php
@@ -1,5 +1,5 @@
 <?php if($this->session->userdata('id_level')>='0'):?> 
-<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <html lang="en"	dir="ltr">
 	<head>
 		<meta charset="utf-8">
