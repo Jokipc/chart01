f63c033973677cd529d5a32b773febbaae9edaeb
jokipc <abagussaputra@gmail.com>
2022-02-02 13:58:47 +0700

    pasarid

--- application/views/pasarid/pasarid_list.php
+++ application/views/pasarid/pasarid_list.php
@@ -52,7 +52,7 @@
                 </form>
             </div>
         </div>
-        <table class="table table-bordered" style="width:90%"">
+        <table class="table table-bordered" style="width:80%"">
             <tr>
                 <th>No</th>
 		<th>Pn</th>
