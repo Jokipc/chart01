0370792261185879271065579ab4652a7cf6b325
jokipc <abagussaputra@gmail.com>
2022-02-02 22:05:58 +0700

    admin

--- application/views/templates/sidebaradmin.php
+++ application/views/templates/sidebaradmin.php
@@ -39,7 +39,7 @@
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-edit"></i>
               <p>
-                Forms
+                Data
                 <i class="fas fa-angle-left right"></i>
               </p>
             </a>
