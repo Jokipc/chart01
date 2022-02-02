fd9d20f9d28b8f32edb65de0d97a026235a66c6e
jokipc <abagussaputra@gmail.com>
2022-02-02 13:16:02 +0700

    pasarid

--- application/views/brimo/brimo_list.php
+++ application/views/brimo/brimo_list.php
@@ -42,7 +42,7 @@
                 </form>
             </div>
         </div>
-        <table class="table table-bordered" style="margin-bottom: 10px">
+        <table class="table table-bordered" style="border:0px; heigth:70%; overflow:auto; float:left; width:50%">
             <tr>
                 <th>No</th>
 		<th>Pn</th>
