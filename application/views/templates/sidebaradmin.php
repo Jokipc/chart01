1c7a06f51782d11110fdadf02e07cbdb9a20e123
jokipc <abagussaputra@gmail.com>
2022-02-02 22:09:43 +0700

    admin

--- application/views/templates/sidebaradmin.php
+++ application/views/templates/sidebaradmin.php
@@ -21,12 +21,12 @@
      
 
       <!-- Sidebar Menu -->
-      <li class="nav-item">
+      
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-               
+                <li class="nav-item"> 
               
          <a href="<?php echo site_url('home') ?>" class="nav-link ">
          <i class="far fa-circle nav-icon"></i> &nbsp; PENCAPAIAN
