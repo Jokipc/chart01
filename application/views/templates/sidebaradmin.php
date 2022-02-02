c9d9b3851467f5926cbb934746e687a61721b722
jokipc <abagussaputra@gmail.com>
2022-02-02 22:04:09 +0700

    admin

--- application/views/templates/sidebaradmin.php
+++ application/views/templates/sidebaradmin.php
@@ -12,7 +12,7 @@
           <img src="<?php echo base_url() ;?>template/dist/img/<?php echo $this->session->userdata('ico');?>" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-        <h2 style="color:white;"> Dashboard 4DX Admin </h2>
+        <h5 style="color:white;"> Dashboard 4DX Admin </h5>
           <a href="#" style="color:orange" class="d-block"><?php echo $this->session->userdata('nama_mantri');?></a>
 
         </div>
