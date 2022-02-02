5857651ea93ad0af259b3a72a104e54b9360f219
jokipc <abagussaputra@gmail.com>
2022-02-03 00:49:34 +0700

    admin

--- application/views/rank.php
+++ application/views/rank.php
@@ -63,7 +63,7 @@
 				$num = 0;
 				foreach( $data as $val){
 					$num++;
-					print '<tr style="width: 70px">
+					print '<tr style="width: 100%">
 								
 								<td class="td-number align-middle">'.$num.'</td>
 								<td class="td-image">
