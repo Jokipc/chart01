f6d973e2a1261e994af36a21b19f808eba7d4157
jokipc <abagussaputra@gmail.com>
2022-02-02 22:32:23 +0700

    admin

--- application/views/brimo/brimo_form.php
+++ application/views/brimo/brimo_form.php
@@ -8,7 +8,7 @@
 <?php $this->load->view('templates/sidebar'); ?>
 <?php endif;?>
 <?php $this->load->view('templates/meta'); ?>
- <title>harviacode.com - codeigniter crud generator</title>
+ <title></title>
         <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>"/>
         <style>
             body{
