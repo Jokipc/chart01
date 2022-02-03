75e0581037b3c0c8465c6995b5441afc01b4762e
jokipc <abagussaputra@gmail.com>
2022-02-04 01:50:13 +0700

    rank

--- application/views/rank.php
+++ application/views/rank.php
@@ -36,7 +36,7 @@ background: linear-gradient(90deg, rgba(2,0,36,0.7123891793045343) 0%, rgba(21,2
 			if ($number <= 3 ){
 				print '<img src="'.base_url().'template/dist/img/a'.$number.'.png" id="balon'.$number.'" class="b b'.$number.'" alt="">';
 				print '<div class="b b'.$number.'" style="color: orange;"><br><br><center>'.$val->unit.'</center></br></br></div>';
-				print '<br><br><br><br>';
+				print '<tr></tr>';
 				print '<div class="b b'.$number.'" style="color: blue;"><br><br><center>'.$val->nama_mantri.'</center></br></br></div>';
 				
 			}
