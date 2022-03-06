876fc09ea6559bb03edab8366b5691528d2457df
Jokipc <abagussaputra@gmail.com>
2022-03-07 00:18:28 +0700

    rumus

--- application/views/rank_ritel.php
+++ application/views/rank_ritel.php
@@ -28,32 +28,47 @@ background: linear-gradient(90deg, rgba(19,200,42,1) 0%, rgba(95,242,218,1) 35%,
 				$nilai1 = array();
 				foreach( $data as $val){
 					$mantri= $val->nama_mantri;
+					$persen = 110/100;
+					
+					$ba = $val->bbankgaransi;
 					$a = $val->real_a;	
-					if($a>110){$a1 = 110;}else{ $a1 = $a;}
+					if($a>110){$a1 = 110; $asen = $ba * $persen;}else{ $a1 = $a; $asen = $a * $ba / 100 ;}
+					$bb = $val->bbristore;
 					$b = $val->real_b;	
-					if($b>110){$b1 = 110;}else{ $b1 = $b;}
+					if($b>110){$b1 = 110;$bsen = $bb * $persen;}else{ $b1 = $b;$bsen = $b * $bb / 100;}
+					$bc = $val->bibbiz;
 					$c = $val->real_c;	
-					if($c>110){$c1 = 110;}else{ $c1 = $c;}
+					if($c>110){$c1 = 110;$csen = $bc * $persen;}else{ $c1 = $c;$csen =$c * $bc / 100;}
+					$bd = $val->bbritama;
 					$d = $val->real_d;	
-					if($d>110){$d1 = 110;}else{ $d1 = $d;}
+					if($d>110){$d1 = 110;$dsen = $bd * $persen;}else{ $d1 = $d;$dsen = $d * $bd / 100;}
+					$be = $val->bpremi;
 					$e = $val->real_e;	
-					if($e>110){$e1 = 110;}else{ $e1 = $e;}
+					if($e>110){$e1 = 110;$esen = $be * $persen;}else{ $e1 = $e;$esen = $e * $be / 100;}
+					$bf = $val->bpenyalurankur;
 					$f = $val->real_f;	
-					if($f>110){$f1 = 110;}else{ $f1 = $f;}
+					if($f>110){$f1 = 110;$fsen = $bf * $persen;}else{ $f1 = $f;$fsen = $f * $bf / 100;}
+					$bg = $val->bbrimo;
 					$g = $val->real_g;	
-					if($g>110){$g1 = 110;}else{ $g1 = $g;}
+					if($g>110){$g1 = 110;$gsen = $bg * $persen;}else{ $g1 = $g;$gsen = $g * $bg / 100;}
+					$bh = $val->bqris;
 					$h = $val->real_h;	
-					if($h>110){$h1 = 110;}else{ $h1 = $h;}
+					if($h>110){$h1 = 110;$hsen = $bh * $persen;}else{ $h1 = $h;$hsen = $h * $bh / 100;}
+					$bi = $val->brealkecil;
 					$i = $val->real_i;	
-					if($i>110){$i1 = 110;}else{ $i1 = $i;}
+					if($i>110){$i1 = 110;$isen = $bi * $persen;}else{ $i1 = $i;$isen = $i * $bi / 100;}
+					$bj = $val->bekstrakom;
 					$j = $val->real_j;	
-					if($j>110){$j1 = 110;}else{ $j1 = $j;}
+					if($j>110){$j1 = 110;$jsen = $bj * $persen;}else{ $j1 = $j;$jsen = $j * $bj / 100;}
+				
+					$totalpersen =$asen+$bsen+$csen+$dsen+$esen+$fsen+$gsen+$hsen+$isen+$jsen;
 			 
 					$total =($a1+$b1+$c1+$d1+$e1+$f1+$g1+$h1+$i1+$j1)/10;
 					array_push($isi1, array('nama_mantri' => $mantri,
 					'hasil' =>$total,
+					'persen'=>$totalpersen,
 					));
-					array_push($nilai1,$total);
+					array_push($nilai1,$totalpersen);
 				}
 				array_multisort($nilai1,SORT_DESC, $isi1);
 				foreach($isi1 as $na)
@@ -63,7 +78,7 @@ background: linear-gradient(90deg, rgba(19,200,42,1) 0%, rgba(95,242,218,1) 35%,
 								
 								<td>'.$num.'.</td>
 								<td>'.$na['nama_mantri'].'&nbsp;</td>
-								<td>'.number_format($na['hasil'],2).'&nbsp;%&nbsp;</td>
+								<td>'.number_format($na['persen'],2).'&nbsp;%&nbsp;</td>
 								
 								
 								
@@ -80,32 +95,47 @@ background: linear-gradient(90deg, rgba(19,200,42,1) 0%, rgba(95,242,218,1) 35%,
 				$nilai1 = array();
 				foreach( $data as $val){
 					$mantri= $val->nama_mantri;
+					$persen = 110/100;
+					
+					$ba = $val->bbankgaransi;
 					$a = $val->real_a;	
-					if($a>110){$a1 = 110;}else{ $a1 = $a;}
+					if($a>110){$a1 = 110; $asen = $ba * $persen;}else{ $a1 = $a; $asen = $a * $ba / 100 ;}
+					$bb = $val->bbristore;
 					$b = $val->real_b;	
-					if($b>110){$b1 = 110;}else{ $b1 = $b;}
+					if($b>110){$b1 = 110;$bsen = $bb * $persen;}else{ $b1 = $b;$bsen = $b * $bb / 100;}
+					$bc = $val->bibbiz;
 					$c = $val->real_c;	
-					if($c>110){$c1 = 110;}else{ $c1 = $c;}
+					if($c>110){$c1 = 110;$csen = $bc * $persen;}else{ $c1 = $c;$csen =$c * $bc / 100;}
+					$bd = $val->bbritama;
 					$d = $val->real_d;	
-					if($d>110){$d1 = 110;}else{ $d1 = $d;}
+					if($d>110){$d1 = 110;$dsen = $bd * $persen;}else{ $d1 = $d;$dsen = $d * $bd / 100;}
+					$be = $val->bpremi;
 					$e = $val->real_e;	
-					if($e>110){$e1 = 110;}else{ $e1 = $e;}
+					if($e>110){$e1 = 110;$esen = $be * $persen;}else{ $e1 = $e;$esen = $e * $be / 100;}
+					$bf = $val->bpenyalurankur;
 					$f = $val->real_f;	
-					if($f>110){$f1 = 110;}else{ $f1 = $f;}
+					if($f>110){$f1 = 110;$fsen = $bf * $persen;}else{ $f1 = $f;$fsen = $f * $bf / 100;}
+					$bg = $val->bbrimo;
 					$g = $val->real_g;	
-					if($g>110){$g1 = 110;}else{ $g1 = $g;}
+					if($g>110){$g1 = 110;$gsen = $bg * $persen;}else{ $g1 = $g;$gsen = $g * $bg / 100;}
+					$bh = $val->bqris;
 					$h = $val->real_h;	
-					if($h>110){$h1 = 110;}else{ $h1 = $h;}
+					if($h>110){$h1 = 110;$hsen = $bh * $persen;}else{ $h1 = $h;$hsen = $h * $bh / 100;}
+					$bi = $val->brealkecil;
 					$i = $val->real_i;	
-					if($i>110){$i1 = 110;}else{ $i1 = $i;}
+					if($i>110){$i1 = 110;$isen = $bi * $persen;}else{ $i1 = $i;$isen = $i * $bi / 100;}
+					$bj = $val->bekstrakom;
 					$j = $val->real_j;	
-					if($j>110){$j1 = 110;}else{ $j1 = $j;}
+					if($j>110){$j1 = 110;$jsen = $bj * $persen;}else{ $j1 = $j;$jsen = $j * $bj / 100;}
+				
+					$totalpersen =$asen+$bsen+$csen+$dsen+$esen+$fsen+$gsen+$hsen+$isen+$jsen;
 			 
 					$total =($a1+$b1+$c1+$d1+$e1+$f1+$g1+$h1+$i1+$j1)/10;
 					array_push($isi1, array('nama_mantri' => $mantri,
 					'hasil' =>$total,
+					'persen'=>$totalpersen,
 					));
-					array_push($nilai1,$total);
+					array_push($nilai1,$totalpersen);
 				}
 				array_multisort($nilai1,SORT_DESC, $isi1);
 				foreach($isi1 as $na)
@@ -165,7 +195,7 @@ background: linear-gradient(90deg, rgba(19,200,42,1) 0%, rgba(95,242,218,1) 35%,
 		<td class="td-value"><center><b>QRIS</b></center></td>
 		<td class="td-value"><center><b>Real Kecil</b></center></td>
 		<td class="td-value"><center><b>Ekstrakom</b></center></td>
-		<td class="td-value"><center><b>Total %</b></center></td>
+		<td class="td-value"><center><b>Total*Bobot %</b></center></td>
 
 
 		</tr>
@@ -179,30 +209,40 @@ background: linear-gradient(90deg, rgba(19,200,42,1) 0%, rgba(95,242,218,1) 35%,
 				foreach( $data as $val){
 					
 					$mantri= $val->nama_mantri;
-
-					$a = $val->real_a;	
-					$ba = $val->b_bankgaransi ;
-					if($a>110){$a1 = 110;}else{ $a1 = $a;}
-					$b = $val->real_b;	
-					if($b>110){$b1 = 110;}else{ $b1 = $b;}
-					$c = $val->real_c;	
-					if($c>110){$c1 = 110;}else{ $c1 = $c;}
-					$d = $val->real_d;	
-					if($d>110){$d1 = 110;}else{ $d1 = $d;}
-					$e = $val->real_e;	
-					if($e>110){$e1 = 110;}else{ $e1 = $e;}
-					$f = $val->real_f;	
-					if($f>110){$f1 = 110;}else{ $f1 = $f;}
-					$g = $val->real_g;	
-					if($g>110){$g1 = 110;}else{ $g1 = $g;}
-					$h = $val->real_h;	
-					if($h>110){$h1 = 110;}else{ $h1 = $h;}
-					$i = $val->real_i;	
-					if($i>110){$i1 = 110;}else{ $i1 = $i;}
-					$j = $val->real_j;	
-					if($j>110){$j1 = 110;}else{ $j1 = $j;}
-				
+					$persen = 110/100;
 					
+					$ba = $val->bbankgaransi;
+					$a = $val->real_a;	
+					if($a>110){$a1 = 110; $asen = $ba * $persen;}else{ $a1 = $a; $asen = $a * $ba / 100 ;}
+					$bb = $val->bbristore;
+					$b = $val->real_b;	
+					if($b>110){$b1 = 110;$bsen = $bb * $persen;}else{ $b1 = $b;$bsen = $b * $bb / 100;}
+					$bc = $val->bibbiz;
+					$c = $val->real_c;	
+					if($c>110){$c1 = 110;$csen = $bc * $persen;}else{ $c1 = $c;$csen =$c * $bc / 100;}
+					$bd = $val->bbritama;
+					$d = $val->real_d;	
+					if($d>110){$d1 = 110;$dsen = $bd * $persen;}else{ $d1 = $d;$dsen = $d * $bd / 100;}
+					$be = $val->bpremi;
+					$e = $val->real_e;	
+					if($e>110){$e1 = 110;$esen = $be * $persen;}else{ $e1 = $e;$esen = $e * $be / 100;}
+					$bf = $val->bpenyalurankur;
+					$f = $val->real_f;	
+					if($f>110){$f1 = 110;$fsen = $bf * $persen;}else{ $f1 = $f;$fsen = $f * $bf / 100;}
+					$bg = $val->bbrimo;
+					$g = $val->real_g;	
+					if($g>110){$g1 = 110;$gsen = $bg * $persen;}else{ $g1 = $g;$gsen = $g * $bg / 100;}
+					$bh = $val->bqris;
+					$h = $val->real_h;	
+					if($h>110){$h1 = 110;$hsen = $bh * $persen;}else{ $h1 = $h;$hsen = $h * $bh / 100;}
+					$bi = $val->brealkecil;
+					$i = $val->real_i;	
+					if($i>110){$i1 = 110;$isen = $bi * $persen;}else{ $i1 = $i;$isen = $i * $bi / 100;}
+					$bj = $val->bekstrakom;
+					$j = $val->real_j;	
+					if($j>110){$j1 = 110;$jsen = $bj * $persen;}else{ $j1 = $j;$jsen = $j * $bj / 100;}
+				
+					$totalpersen =$asen+$bsen+$csen+$dsen+$esen+$fsen+$gsen+$hsen+$isen+$jsen;
 					$total =($a1+$b1+$c1+$d1+$e1+$f1+$g1+$h1+$i1+$j1)/10;
 					array_push($isi, array('nama_mantri' => $mantri,
 					'bankgaransi' => $a1,
@@ -215,10 +255,10 @@ background: linear-gradient(90deg, rgba(19,200,42,1) 0%, rgba(95,242,218,1) 35%,
 					'qris' => $h1,
 					'realkecil' => $i1,
 					'ekstrakom' => $j1,
-
+					'persen'=>$totalpersen,
 					'hasil' =>$total,
 					));
-					array_push($nilai,$total);
+					array_push($nilai,$totalpersen);
 				}
 				array_multisort($nilai,SORT_DESC, $isi);
 				foreach($isi as $n)
@@ -237,7 +277,7 @@ background: linear-gradient(90deg, rgba(19,200,42,1) 0%, rgba(95,242,218,1) 35%,
 								<td class="td-name">'.$n['nama_mantri'].'</td>
 							
 								<td class="td-value">'.number_format($n['bankgaransi'],0).'%</td>
-								<td class="td-value">'.number_format($n['bristore'],0).'%</td>
+								<td class="td-value">'.number_format($n['bristore'],2).'%</td>
 								<td class="td-value">'.number_format($n['ibbiz'],0).'%</td>
 								<td class="td-value">'.number_format($n['britamabisnis'],0).'%</td>
 								<td class="td-value">'.number_format($n['premi'],0).'%</td>
@@ -246,7 +286,7 @@ background: linear-gradient(90deg, rgba(19,200,42,1) 0%, rgba(95,242,218,1) 35%,
 								<td class="td-value">'.number_format($n['qris'],0).'%</td>
 								<td class="td-value">'.number_format($n['realkecil'],0).'%</td>
 								<td class="td-value">'.number_format($n['ekstrakom'],0).'%</td>
-								<td class="td-value">'.number_format($n['hasil'],2).'%</td>
+								<td class="td-value"><b>'.number_format($n['persen'],2).'%</b></td>
 								</tr>';		
 					}
 					else {}
