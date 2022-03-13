ed1858667caf32d88c3f2e5bef2a50b2f9f746cb
Jokipc <abagussaputra@gmail.com>
2022-03-14 02:42:40 +0700

    search

--- application/views/bintang.php
+++ application/views/bintang.php
@@ -1,5 +1,18 @@
 
+<!doctype html>
+<html>
+<head>
+<?php $this->load->view('templates/js'); ?>
+<?php $this->load->view('templates/header'); ?>
+<?php $this->load->view('templates/meta'); ?>
+<?php if($this->session->userdata('side')==='3' ):?> 
+<?php $this->load->view('templates/sidebaradminunit'); ?>
+<?php elseif($this->session->userdata('id_level')==='1' ): ?>
+<?php $this->load->view('templates/sidebaradmin'); ?>
 
+
+<?php else: ?>
+<?php endif;?>
 
 
 
