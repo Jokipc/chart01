70028677c35fbdfc8845eed063dd099c9dcfb116
Jokipc <abagussaputra@gmail.com>
2022-03-27 21:52:49 +0700

    menudana

--- application/views/templates/home_dana.php
+++ application/views/templates/home_dana.php
@@ -28,23 +28,25 @@
       $bj = $val->b_digitalsav;
       ?>
           
+          <?php $this->load->view('templates/js') ?>
 <?php $this->load->view('templates/header') ?>
 
 
 
 <div class="wrapper">
 
- 
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="<?php echo base_url() ;?>template/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+  </div>
 
   <!-- Navbar -->
  <?php $this->load->view('templates/meta') ?>
- 
- <!-- /.navbar -->
+  <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
 
-<?php $this->load->view('templates/js'); ?>
-
+<?php $this->load->view('templates/sidebaradminritel'); ?>
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper" style="background-color:white";>
